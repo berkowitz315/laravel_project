9664622dae4dedf01b0392cdd8c10106945220a5
Navjot Singh <weavebytes@gmail.com>
2015-11-28 23:15:43 +0530

    adding fillable list for Project model

--- app/Project.php
+++ app/Project.php
@@ -4,8 +4,22 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Task;
+
 class Project extends Model
 {
+
+	/**
+     * Fillable fields
+     * 
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+
     public function tasks()
     {
         return $this->hasMany('App\Task');
