edfc05923d7a0e0505bef19bfcb0a63648be99ee
Navjot Singh <weavebytes@gmail.com>
2015-12-02 20:05:17 +0530

    enhanced partials/forms for project and task

--- resources/views/projects/partials/_form.blade.php
+++ resources/views/projects/partials/_form.blade.php
@@ -1,11 +1,13 @@
-<div class="form-group">
-    {!! Form::label('name', 'Name:') !!}
-    {!! Form::text('name') !!}
-</div>
-<div class="form-group">
-    {!! Form::label('slug', 'Slug:') !!}
-    {!! Form::text('slug') !!}
-</div>
-<div class="form-group">
-    {!! Form::submit($submit_text, ['class'=>'btn primary']) !!}
+<div style="width:400px; ">
+	<div class="form-group">
+	    {!! Form::label('name', 'Project Name:') !!}
+    	{!! Form::text('name', null, array('class' => 'form-control', 'placeholder' => 'Project Name')) !!}
+	</div>
+	<div class="form-group">
+    	{!! Form::label('slug', 'Slug:') !!}
+    	{!! Form::text('slug', null, array('class' => 'form-control', 'placeholder' => 'Slug')) !!}
+	</div>
+	<div class="form-group">
+    	{!! Form::submit($submit_text, ['class'=>'btn primary']) !!}
+	</div>
 </div>
--- resources/views/tasks/partials/_form.blade.php
+++ resources/views/tasks/partials/_form.blade.php
@@ -1,23 +1,25 @@
-<div class="form-group">
-    {!! Form::label('name', 'Name:') !!}
-    {!! Form::text('name') !!}
-</div>
+<div style="width:450px; ">
+	<div class="form-group">
+    	{!! Form::label('name', 'Name:') !!}
+    	{!! Form::text('name', null, array('class' => 'form-control', 'placeholder' => 'Task Name')) !!}
+	</div>
  
-<div class="form-group">
-    {!! Form::label('slug', 'Slug:') !!}
-    {!! Form::text('slug') !!}
-</div>
+	<div class="form-group">
+    	{!! Form::label('slug', 'Slug:') !!}
+    	{!! Form::text('slug', null, array('class' => 'form-control', 'placeholder' => 'Slug')) !!}
+	</div>
  
-<div class="form-group">
-    {!! Form::label('completed', 'Completed:') !!}
-    {!! Form::checkbox('completed') !!}
-</div>
+	<div class="form-group">
+    	{!! Form::label('completed', 'Completed:') !!}
+    	{!! Form::checkbox('completed') !!}
+	</div>
  
-<div class="form-group">
-    {!! Form::label('description', 'Description:') !!}
-    {!! Form::textarea('description') !!}
-</div>
+	<div class="form-group">
+    	{!! Form::label('description', 'Description:') !!}
+    	{!! Form::textarea('description', null, array('class' => 'form-control', 'placeholder' => 'Enter Task Description Here...')) !!}
+	</div>
  
-<div class="form-group">
-    {!! Form::submit($submit_text) !!}
+	<div class="form-group">
+    	{!! Form::submit($submit_text) !!}
+	</div>
 </div>
